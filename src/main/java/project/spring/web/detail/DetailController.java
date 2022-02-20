50eeace7d612583d38ca6ca7e5e2518bc4bb8092
Yeji Lee <fpdlsqhdnvj@naver.com>
2022-02-20 23:13:29 +0900

    subscribe mapping 추가 - 중간에 멈춤-

--- src/main/java/project/spring/web/detail/DetailController.java
+++ src/main/java/project/spring/web/detail/DetailController.java
@@ -1,5 +1,8 @@
 package project.spring.web.detail;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import project.spring.web.mapping.MappingService;
+import project.spring.web.mapping.MappingVO;
 import project.spring.web.review.WriteReviewService;
 import project.spring.web.review.WriteReviewVO;
 import project.spring.web.tapPage.TapPageService;
@@ -23,6 +28,10 @@ public class DetailController {
 	private TapPageService tapPageService;
 	@Autowired
 	private WriteReviewService writeReviewService;
+	@Autowired
+	private MappingService mappingService;
+	
+	
 	public DetailController() {
 		System.out.println("detailController 실행");
 	}
@@ -37,15 +46,32 @@ public class DetailController {
 		String a = request.getParameter("tagMain01");
 		int tagMain01 = Integer.parseInt(a);
 		System.out.println("넘어온 tagMain01 값 : "+ tagMain01);
-//		아이템 상품정보
-		DetailVO VO1 = new DetailVO();
-		VO1.setItem_code(menuNum);
-		mav.addObject("detail", detailService.getItem(VO1));
-		mav.addObject("detailInfo", detailService.getInfo(VO1));
-//		아이템 평균별점/게시글카운트
-		TapPageVO VO2 = new TapPageVO();
-		VO2.setItemCode(menuNum);
-		mav.addObject("avgCount", tapPageService.getAvgCount(VO2));
+		//단품상품일때
+		if(tagMain01 != 100 && tagMain01 != 600) {
+			//단품상품일때
+//			아이템 상품정보			
+			DetailVO VO1 = new DetailVO();
+			VO1.setItem_code(menuNum);
+			mav.addObject("detail", detailService.getItem(VO1));
+			mav.addObject("detailInfo", detailService.getInfo(VO1));
+//			아이템 평균별점/게시글카운트
+			TapPageVO VO2 = new TapPageVO();
+			VO2.setItemCode(menuNum);
+			mav.addObject("avgCount", tapPageService.getAvgCount(VO2));
+		} else {
+			//세트 or 구독상품일때
+			MappingVO mapVO = new MappingVO();
+			mapVO.setSubscribe_code(menuNum);			
+			
+			List<DetailVO> itemcodes = new ArrayList<DetailVO>();
+			
+			List<MappingVO> aa = mappingService.getItemCode(mapVO);
+            for(MappingVO vo : aa) {
+            	DetailVO dvo = new DetailVO();
+            	dvo.setItem_code(vo.getItem_code());
+            	itemcodes.add(detailService.getItem(dvo));
+            }
+		}
 		
 //		리뷰보드
 		WriteReviewVO VO3 = new WriteReviewVO();

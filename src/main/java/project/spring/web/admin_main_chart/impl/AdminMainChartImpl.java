ec0831be520be5f7cbc8f70a1efee0ff22368a63
DESKTOP-36BD3PV\Leo <packc38@gmail.com>
2022-03-21 10:39:09 +0900

    a

---
+++ src/main/java/project/spring/web/admin_main_chart/impl/AdminMainChartImpl.java
@@ -0,0 +1,7 @@
+package project.spring.web.admin_main_chart.impl;
+
+import project.spring.web.admin_main_chart.AdminMainChartService;
+
+public class AdminMainChartImpl implements AdminMainChartService {
+
+}

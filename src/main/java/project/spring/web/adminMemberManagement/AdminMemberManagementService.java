d8756b1de2b11862f0c5be59ad121e0c044143b1
jjaehun <qwert12323@naver.com>
2022-03-22 10:46:05 +0900

    ww

---
+++ src/main/java/project/spring/web/adminMemberManagement/AdminMemberManagementService.java
@@ -0,0 +1,9 @@
+package project.spring.web.adminMemberManagement;
+
+import java.util.List;
+
+public interface AdminMemberManagementService {
+
+	public List<AdminMemberManagementVO> getAdminMember(AdminMemberManagementVO vo);
+
+}

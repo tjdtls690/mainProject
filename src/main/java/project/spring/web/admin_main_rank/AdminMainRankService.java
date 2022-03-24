66bf02efd0b718b1191675c261edf7830468351b
DESKTOP-36BD3PV\Leo <packc38@gmail.com>
2022-03-24 13:13:22 +0900

    ..

---
+++ src/main/java/project/spring/web/admin_main_rank/AdminMainRankService.java
@@ -0,0 +1,7 @@
+package project.spring.web.admin_main_rank;
+
+public interface AdminMainRankService {
+	
+//	List<AdminRankVO> 
+
+}
---
+++ src/main/java/project/spring/web/admin_main_rank/impl/AdminMainRankServiceImpl.java
@@ -0,0 +1,7 @@
+package project.spring.web.admin_main_rank.impl;
+
+import project.spring.web.admin_main_rank.AdminMainRankService;
+
+public class AdminMainRankServiceImpl implements AdminMainRankService {
+
+}

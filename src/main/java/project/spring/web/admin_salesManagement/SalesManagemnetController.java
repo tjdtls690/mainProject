2724b45147663f3d972493ce6dfb18e5c7101edd
DESKTOP-36BD3PV\Leo <packc38@gmail.com>
2022-03-25 17:27:07 +0900

    통계 셋팅

--- src/main/java/project/spring/web/admin_salesManagement/SalesManagemnetController.java
+++ src/main/java/project/spring/web/admin_salesManagement/SalesManagemnetController.java
@@ -1,5 +1,7 @@
 package project.spring.web.admin_salesManagement;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,4 +15,34 @@ public class SalesManagemnetController {
 		mav.setViewName("salesManagement");
 		return mav;	
 	}
+	
+	@RequestMapping("/getMonthList.mdo")
+	public ModelAndView monthList(HttpServletRequest request,ModelAndView mav) {
+		String getMonth = request.getParameter("getMonth");
+		char month =getMonth.charAt(3);
+		System.out.println("가져올 월 : "+month);
+		
+		
+		if(month =='1') {
+			System.out.println("1개월임");
+			
+			
+		}else if(month =='3') {
+			System.out.println("3개월임");
+			
+			
+		}else {
+			System.out.println("6개월임");
+			
+			
+		}
+		
+		
+		return mav;
+	}
+	
+	
+	
+	
+	
 }

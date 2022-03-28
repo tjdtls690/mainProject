27f44923c05f457f0f26fa60188b347e47d7a565
Yeji Lee <fpdlsqhdnvj@naver.com>
2022-03-28 14:06:00 +0900

    이제진짜됐겠지...^^...

--- src/main/java/project/spring/web/admin_review/AdminReviewController.java
+++ src/main/java/project/spring/web/admin_review/AdminReviewController.java
@@ -1,5 +1,6 @@
 package project.spring.web.admin_review;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -33,7 +34,7 @@ public class AdminReviewController {
 		List<AdminReviewVO> subReviewList = adminReviewService.getAdminSubList(vo1);
 		mav.addObject("subReviewList",subReviewList);
 		
-		mav.setViewName("reviewList");
+		mav.setViewName("reviewListAdmin");
 		return mav;
 	}
 	
@@ -48,7 +49,26 @@ public class AdminReviewController {
 		vo1.setReply("y");
 		List<AdminReviewVO> subReviewList = adminReviewService.getAdminSubList(vo1);
 		mav.addObject("subReviewList",subReviewList);
-		mav.setViewName("reviewManagement");
+		
+		ReviewReplyVO rvo2 = new ReviewReplyVO();
+		List<ReviewReplyVO> itemReplyList = new ArrayList<ReviewReplyVO>();
+		for(int i = 0; i < itemReviewList.size(); i++) {
+			rvo2.setSeq(itemReviewList.get(i).getSeq());
+			ReviewReplyVO getReply = adminReviewService.getReviewReply(rvo2);
+			itemReplyList.add(getReply);
+		}
+		
+		ReviewReplyVO rvo3 = new ReviewReplyVO();
+		List<ReviewReplyVO> subReplyList = new ArrayList<ReviewReplyVO>();
+		for(int i = 0; i < subReviewList.size(); i++) {
+			rvo3.setSeq(subReviewList.get(i).getSeq());
+			ReviewReplyVO getReply = adminReviewService.getReviewReply(rvo3);
+			subReplyList.add(getReply);
+		}
+		
+		mav.addObject("itemReplyList", itemReplyList);
+		mav.addObject("subReplyList",subReplyList);
+		mav.setViewName("reviewManagementAdmin");
 		return mav;
 	}
 	
@@ -59,28 +79,16 @@ public class AdminReviewController {
 		String reply = request.getParameter("reply_content");
 		System.out.println("seq : " + seq + "reply : " + reply);
 		
-		//review�떟湲� �뀒�씠釉붿뿉 insert
 		ReviewReplyVO rvo = new ReviewReplyVO();
 		rvo.setSeq(seq);
 		rvo.setReplycontents(reply);
 		adminReviewService.reviewReplyInsert(rvo);
 		
-		//reviewboard�뀒�씠釉붿뿉 reply瑜� y濡� 諛붽씀湲�
 		AdminReviewVO avo = new AdminReviewVO();
 		avo.setSeq(seq);
 		adminReviewService.reviewUpdate(avo);
 		
-		AdminReviewVO vo = new AdminReviewVO();
-		vo.setReply("y");
-		List<AdminReviewVO> itemReviewList = adminReviewService.getAdminItemList(vo);
-		mav.addObject("itemReviewList",itemReviewList);
-		
-		AdminReviewVO vo1 = new AdminReviewVO();
-		vo1.setReply("y");
-		List<AdminReviewVO> subReviewList = adminReviewService.getAdminSubList(vo1);
-		mav.addObject("subReviewList",subReviewList);
-		
-		mav.setViewName("reviewManagement");
+		mav.setViewName("reviewManagementAdmin");
 		return mav;
 	}
 }
